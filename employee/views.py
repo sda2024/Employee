4f58bc52b222a7044cb9b6b6c1207c3fe944f1bd
jyaken <jyaken@163.com>
2024-03-06 11:54:42 +0800

    Read employee info from DB.

--- employee/views.py
+++ employee/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Employee
 
 def index(request):
-    return HttpResponse("<h1>Hello from employee</h1>")
+    template = loader.get_template('index.html')
+    myEmployees =Employee.objects.all().values()
+    context = {
+        "myEmployees": myEmployees,
+    }
+    return HttpResponse(template.render(context,request))
